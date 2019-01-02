f56153c40620027c9335c4081224fbfc50551a6f
Anderson Nishihara <nishihara@gmail.com>
2019-01-02 15:06:51 -0200

    Fix bug: Not creating folder. Add more instructions.

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 # !/bin/bash
 
-mkdir ~/.config/nvim
+mkdir -p .config/nvim
 cp init.vim ~/.config/nvim/init.vim
+pip3 install --user neovim
 npm install -g yarn
 npm install -g typescript
 npm install -g babel-eslint
 npm install -g eslint
+
+echo "Open neovim and execute: ':PlugInstall'."
+
+
